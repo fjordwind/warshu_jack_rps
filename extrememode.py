b763db88a4d82818aee76423ea60e5c96c5da472
Fjord <103873047+fjordwind@users.noreply.github.com>
2024-11-01 19:29:36 -0700

    Update extrememode.py

--- extrememode.py
+++ extrememode.py
@@ -21,8 +21,52 @@ def playerChoiceExtreme():
         if userInput in [choiceextreme.name for choiceextreme in choiceExtreme]:
             return choiceExtreme[userInput.upper()]
         elif userInput == "Fahrenheit 451":
-            print("Didn't you mean PARCHMENT, or FLAME?")
+            print("Didn't you mean",6 "or" ,11"?")
             return choiceExtreme[11]
         else:
             print("Invalid input, try again.")
             continue
+
+
+def cpuChoice():
+    return random.choice(list(choiceExtreme))
+
+def winningPlayer(playerChoiceExxtreme, cpuChoice):
+    listOfWinners = {
+        ## rock wins
+        (choiceExtreme.SLATE, choiceExtreme.CLEAVER): "USER",
+        (choiceExtreme.SLATE, choiceExtreme.FLAME): "USER",
+        (choiceExtreme.SLATE, choiceExtreme.WARSHU): "USER",
+        (choiceExtreme.SLATE, choiceExtreme.DAWG): "USER",
+        (choiceExtreme.SLATE, choiceExtreme.PORIFERA): "USER",
+        ## fire wins
+        (choiceExtreme.FLAME, choiceExtreme.CLEAVER): "USER",
+        (choiceExtreme.FLAME, choiceExtreme.PARCHMENT): "USER",
+        (choiceExtreme.FLAME, choiceExtreme.WARSHU): "USER",
+        (choiceExtreme.FLAME, choiceExtreme.DAWG): "USER",
+        (choiceExtreme.FLAME, choiceExtreme.PORIFERA): "USER",
+        ## scissors wins
+        (choiceExtreme.CLEAVER, choiceExtreme.ATMOSPHERE): "USER",
+        (choiceExtreme.CLEAVER, choiceExtreme.PARCHMENT): "USER",
+        (choiceExtreme.CLEAVER, choiceExtreme.WARSHU): "USER",
+        (choiceExtreme.CLEAVER, choiceExtreme.DAWG): "USER",
+        (choiceExtreme.CLEAVER, choiceExtreme.PORIFERA): "USER",
+        ## human wins
+        (choiceExtreme.WARSHU, choiceExtreme.DAWG): "USER",
+        (choiceExtreme.WARSHU, choiceExtreme.POLIFERA): "USER",
+        (choiceExtreme.WARSHU, choiceExtreme.PARCHEMENT): "USER",
+        (choiceExtreme.WARSHU, choiceExtreme.ATMOSPHERE): "USER",
+        (choiceExtreme.WARSHU, choiceExtreme.OXIDANE): "USER",
+        ## wolf wins
+        (choiceExtreme.DAWG, choiceExtreme.POLIFERA): "USER",
+        (choiceExtreme.DAWG, choiceExtreme.PARCHMENT): "USER",
+        (choiceExtreme.DAWG, choiceExtreme.DAEMON): "USER",
+        (choiceExtreme.DAWG, choiceExtreme.ATMOSPHERE): "USER",
+        (choiceExtreme.DAWG, choiceExtreme.OXIDANE): "USER",
+        ## sponge wins
+        (choiceExtreme.POLIFERA, choiceExtreme.GLOCK): "USER",
+        (choiceExtreme.PORIFERA, choiceExtreme.PARCHMENT): "USER",
+        (choiceExtreme.PORIFERA, choiceExtreme.DAEMON): "USER",
+        (choiceExtreme.PORIFERA, choiceExtreme.ATMOSPHERE): "USER",
+        (choiceExtreme.PORIFERA, choiceExtreme.OXIDANE): "USER",
+    }
